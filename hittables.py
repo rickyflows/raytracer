46c28c0cae7f5e9ad5194285c35964ef568af42a
rickyflows <usetheflowsricky@gmail.com>
2023-12-03 12:05:53 -0800

    Clean up file structure

---
+++ hittables.py
@@ -0,0 +1,131 @@
+import numpy as np
+from utils import Ray, Interval
+
+from numpy.typing import NDArray
+from typing import List
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
+@dataclass
+class HitRecord:
+    def __init__(
+        self,
+        hits: NDArray[np.bool_],  # shape: (m, n)
+        times: NDArray[np.float64],  # shape: (m, n)
+        points: NDArray[np.float64],  # shape: (m, n, 3)
+        normals: NDArray[np.float64],  # shape: (m, n, 3)
+        front_face: NDArray[np.bool_],  # shape: (m, n)
+    ):
+        # If hits[i, j] is false then there no guarantees on other values at i, j
+        self.hits = hits
+        self.times = times
+        self.points = points
+        self.normals = normals
+        self.front_face = front_face
+
+    @classmethod
+    def init_with_ray(cls, hits, times, points, ray, outward_normals):
+        normals, front_face = cls.get_face_normals(ray, outward_normals)
+        return cls(hits, times, points, normals, front_face)
+
+    @staticmethod
+    def get_face_normals(ray: Ray, outward_normals: NDArray):
+        # Sets the hit record normal vector.
+        # NOTE: the parameter `outward_normal` is assumed to have unit length.
+        front_face = np.einsum("ijk,ijk->ij", ray.direction, outward_normals) < 0
+        normals = np.where(
+            front_face[:, :, np.newaxis], outward_normals, -outward_normals
+        )
+        return normals, front_face
+
+
+class Hittable(ABC):
+    @abstractmethod
+    def hit(self, ray: Ray, ray_t: Interval = Interval()) -> HitRecord:
+        pass
+
+
+class HittableList(Hittable):
+    def __init__(self, objects: List[Hittable] = []):
+        self.objects = objects
+
+    def add(self, object):
+        self.objects.append(object)
+
+    def hit(self, ray: Ray, ray_t: Interval = Interval()) -> HitRecord:
+        # TODO: memory optimization and ray_t optimization
+        N = len(self.objects)
+        record_list = []
+        for object in self.objects:
+            record_list.append(object.hit(ray, ray_t))
+
+        hits_stacked = np.stack([record.hits for record in record_list])
+        masked_times_stacked = np.ma.stack(
+            [
+                np.ma.masked_array(record_list[i].times, mask=~hits_stacked[i])
+                for i in range(N)
+            ]
+        )
+        min_indices = np.ma.argmin(masked_times_stacked, axis=0)
+
+        hits_final = np.any(hits_stacked, axis=0)
+        times_final = np.ma.filled(masked_times_stacked.min(axis=0), fill_value=np.inf)
+        m, n = hits_final.shape
+
+        # Calculate remaining values
+        points_list = [record.points for record in record_list]
+        normals_list = [record.normals for record in record_list]
+        front_face_list = [record.front_face for record in record_list]
+
+        points_final = np.empty(shape=(m, n, 3), dtype=np.float64)
+        normals_final = np.empty(shape=(m, n, 3), dtype=np.float64)
+        front_face_final = np.empty(shape=(m, n), dtype=np.bool_)
+        for i in range(N):
+            mask = min_indices == i
+            points_final[mask] = points_list[i][mask]
+            normals_final[mask] = normals_list[i][mask]
+            front_face_final[mask] = front_face_list[i][mask]
+
+        return HitRecord(
+            hits=hits_final,
+            times=times_final,
+            points=points_final,
+            normals=normals_final,
+            front_face=front_face_final,
+        )
+
+
+class Sphere(Hittable):
+    def __init__(self, center: NDArray, radius: float):
+        self.center = center
+        self.radius = radius
+
+    def hit(self, ray: Ray, ray_t: Interval = Interval()) -> HitRecord:
+        oc = ray.origin - self.center
+        a = np.sum(ray.direction**2, axis=-1)
+        half_b = np.tensordot(oc, ray.direction, axes=(-1, -1))
+        c = np.dot(oc, oc) - self.radius * self.radius
+        discriminant = half_b**2 - a * c
+
+        # find the nearest root in the range [ray_t.mins, ray_t.maxs]
+        hits = discriminant >= 0
+        sqrtd = np.sqrt(np.where(hits, discriminant, 0))
+        smaller_roots = (-half_b - sqrtd) / a
+        times = np.where(
+            ~ray_t.surrounds(smaller_roots),
+            (-half_b + sqrtd) / a,
+            smaller_roots,
+        )
+        hits &= ray_t.surrounds(times)
+        points = ray.at(times[:, :, np.newaxis])
+        outward_normals = (points - self.center) / self.radius
+
+        return HitRecord.init_with_ray(
+            hits=hits,
+            times=times,
+            points=points,
+            ray=ray,
+            outward_normals=outward_normals,
+        )
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import *
+import time
+from utils import Ray, unit_vector, vec, color
+from hittables import Sphere, HittableList
 
-np.set_printoptions(precision=2, suppress=True)
+from numpy.typing import NDArray
 
-def ray_color(ray: Ray):
+
+def ray_color(ray: Ray, world: HittableList):
     color_arr = np.empty_like(ray.direction)
     # (m, n, 3) / (m, n)
     unit_directions = unit_vector(ray.direction)
@@ -13,30 +16,41 @@ def ray_color(ray: Ray):
     a = 0.5 * (unit_directions[:, :, 1:2] + 1.0)
     color_arr = (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0)
 
-    logical_hit_sphere = hit_sphere(vec(0, 0, -1), 0.5, ray)
-    color_arr[logical_hit_sphere==True, :] = color(1,0,0)
+    # draw sphere
+    hit_record = world.hit(ray)
+    color_arr[hit_record.hits, :] = (0.5 * (1 + hit_record.normals))[hit_record.hits, :]
     return color_arr
 
-def hit_sphere(center: np.ndarray, radius: float, ray: Ray):
+
+def hit_sphere(center: NDArray, radius: float, ray: Ray):
     oc = ray.origin - center
-    b = 2 * np.tensordot(oc, ray.direction, axes=(0,2))
+    b = 2 * np.tensordot(oc, ray.direction, axes=(0, 2))
     a = np.sum(np.copy(ray.direction**2), axis=2)
     c = np.dot(oc, oc) - radius * radius
+
     discriminant = b**2 - 4 * a * c
-    return (discriminant >= 0)
+    root = (-b - np.sqrt(discriminant)) / (2 * a)
+    root[discriminant < 0] = -1
+    return root
+
 
 def main():
     # Image
     aspect_ratio = 16 / 9
-    image_width = 400
+    image_width = 1200
     image_height = int(image_width / aspect_ratio)
     image_channels = 3
+
+    # World
+    world = HittableList()
+    world.add(Sphere(vec(0, 0, -1), 0.5))
+    world.add(Sphere(vec(0, -100.5, -1), 100))
 
     # Camera
     focal_length = 1.0
     viewport_height = 2.0
     viewport_width = viewport_height * image_width / image_height
-    camera_center = vec(0.0,0.0,0.0)
+    camera_center = vec(0.0, 0.0, 0.0)
 
     # Calculate the vectors across the horizontal and down the vertical viewport edges.
     viewport_u = vec(viewport_width, 0.0, 0.0)
@@ -47,23 +61,32 @@ def main():
     pixel_delta_v = viewport_v / image_height
 
     # Calculate the location of the upper left pixel.
-    viewport_upper_left = camera_center - vec(0.0, 0.0, focal_length) - viewport_u/2 - viewport_v/2
+    viewport_upper_left = (
+        camera_center - vec(0.0, 0.0, focal_length) - viewport_u / 2 - viewport_v / 2
+    )
     pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v)
 
     # Create image
     image = np.zeros((image_height, image_width, image_channels))
     image_indices = np.indices((image_height, image_width)).transpose(1, 2, 0)
 
-    pixel_centers = pixel00_loc + pixel_delta_u * image_indices[:, :, 1:2] + pixel_delta_v * image_indices[:, :, 0:1]
+    pixel_centers = (
+        pixel00_loc
+        + pixel_delta_u * image_indices[:, :, 1:2]
+        + pixel_delta_v * image_indices[:, :, 0:1]
+    )
     ray_directions = pixel_centers - camera_center
     ray = Ray(camera_center, ray_directions)
-    image = ray_color(ray)
-
+    print("Starting image render...")
+    start_time = time.time()
+    image = ray_color(ray, world)
+    print("Time to render image:", time.time() - start_time)
 
     plt.imshow(image)
-    plt.savefig('renders/ray_with_sphere.png')
+    plt.axis("off")
+    plt.savefig("renders/ray_with_sphere_normals.png", bbox_inches="tight")
     plt.show()
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
